871f282c8171b1d6b6842ab99a527cee50471079
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2019-08-31 09:43:57 -0700

    update

---
+++ mtl.py
@@ -0,0 +1,150 @@
+"""
+desitarget.mtl
+==============
+
+Merged target lists.
+"""
+
+import numpy as np
+import sys
+from astropy.table import Table
+
+from desitarget.targetmask import obsmask, obsconditions
+from desitarget.targets import calc_priority, main_cmx_or_sv, set_obsconditions
+
+
+def make_mtl(targets, zcat=None, trim=False):
+    """Adds NUMOBS, PRIORITY, and OBSCONDITIONS columns to a targets table.
+
+    Parameters
+    ----------
+    targets : :class:`~numpy.array` or `~astropy.table.Table`
+        A numpy rec array or astropy Table with at least the columns
+        ``TARGETID``, ``DESI_TARGET``, ``NUMOBS_INIT``, ``PRIORITY_INIT``.
+        or the corresponding columns for SV or commissioning.
+    zcat : :class:`~astropy.table.Table`, optional
+        Redshift catalog table with columns ``TARGETID``, ``NUMOBS``, ``Z``,
+        ``ZWARN``.
+    trim : :class:`bool`, optional
+        If ``True`` (default), don't include targets that don't need
+        any more observations.  If ``False``, include every input target.
+
+    Returns
+    -------
+    :class:`~astropy.table.Table`
+        MTL Table with targets columns plus:
+
+        * NUMOBS_MORE    - number of additional observations requested
+        * PRIORITY       - target priority (larger number = higher priority)
+        * OBSCONDITIONS  - replaces old GRAYLAYER
+    """
+    # ADM set up the default logger.
+    from desiutil.log import get_logger
+    log = get_logger()
+
+    # ADM determine whether the input targets are main survey, cmx or SV.
+    colnames, masks, survey = main_cmx_or_sv(targets)
+    # ADM set the first column to be the "desitarget" column
+    desi_target, desi_mask = colnames[0], masks[0]
+
+    # Trim targets from zcat that aren't in original targets table
+    if zcat is not None:
+        ok = np.in1d(zcat['TARGETID'], targets['TARGETID'])
+        num_extra = np.count_nonzero(~ok)
+        if num_extra > 0:
+            log.warning("Ignoring {} zcat entries that aren't "
+                        "in the input target list".format(num_extra))
+            zcat = zcat[ok]
+
+    n = len(targets)
+    # ADM if the input target columns were incorrectly called NUMOBS or PRIORITY
+    # ADM rename them to NUMOBS_INIT or PRIORITY_INIT.
+    # ADM Note that the syntax is slightly different for a Table.
+    for name in ['NUMOBS', 'PRIORITY']:
+        if isinstance(targets, Table):
+            try:
+                targets.rename_column(name, name+'_INIT')
+            except KeyError:
+                pass
+        else:
+            targets.dtype.names = [name+'_INIT' if col == name else col for col in targets.dtype.names]
+
+    # ADM if a redshift catalog was passed, order it to match the input targets
+    # ADM catalog on 'TARGETID'.
+    if zcat is not None:
+        # ADM there might be a quicker way to do this?
+        # ADM set up a dictionary of the indexes of each target id.
+        d = dict(tuple(zip(targets["TARGETID"], np.arange(n))))
+        # ADM loop through the zcat and look-up the index in the dictionary.
+        zmatcher = np.array([d[tid] for tid in zcat["TARGETID"]])
+        ztargets = zcat
+        if ztargets.masked:
+            unobs = ztargets['NUMOBS'].mask
+            ztargets['NUMOBS'][unobs] = 0
+            unobsz = ztargets['Z'].mask
+            ztargets['Z'][unobsz] = -1
+            unobszw = ztargets['ZWARN'].mask
+            ztargets['ZWARN'][unobszw] = -1
+    else:
+        ztargets = Table()
+        ztargets['TARGETID'] = targets['TARGETID']
+        ztargets['NUMOBS'] = np.zeros(n, dtype=np.int32)
+        ztargets['Z'] = -1 * np.ones(n, dtype=np.float32)
+        ztargets['ZWARN'] = -1 * np.ones(n, dtype=np.int32)
+        # ADM if zcat wasn't passed, there is a one-to-one correspondence
+        # ADM between the targets and the zcat.
+        zmatcher = np.arange(n)
+
+    # ADM extract just the targets that match the input zcat.
+    targets_zmatcher = targets[zmatcher]
+
+    # ADM use passed value of NUMOBS_INIT instead of calling the memory-heavy calc_numobs.
+    # ztargets['NUMOBS_MORE'] = np.maximum(0, calc_numobs(ztargets) - ztargets['NUMOBS'])
+    ztargets['NUMOBS_MORE'] = np.maximum(0, targets_zmatcher['NUMOBS_INIT'] - ztargets['NUMOBS'])
+
+    # ADM we need a minor hack to ensure that BGS targets are observed once (and only once)
+    # ADM every time, regardless of how many times they've previously been observed.
+    # ADM I've turned this off for commissioning. Not sure if we'll keep it in general.
+    if survey != 'cmx':
+        ii = targets_zmatcher[desi_target] & desi_mask.BGS_ANY > 0
+        ztargets['NUMOBS_MORE'][ii] = 1
+
+    # ADM assign priorities, note that only things in the zcat can have changed priorities.
+    # ADM anything else will be assigned PRIORITY_INIT, below.
+    priority = calc_priority(targets_zmatcher, ztargets)
+
+    # If priority went to 0==DONOTOBSERVE or 1==OBS or 2==DONE, then NUMOBS_MORE should also be 0.
+    # ## mtl['NUMOBS_MORE'] = ztargets['NUMOBS_MORE']
+    ii = (priority <= 2)
+    log.info('{:d} of {:d} targets have priority zero, setting N_obs=0.'.format(np.sum(ii), n))
+    ztargets['NUMOBS_MORE'][ii] = 0
+
+    # - Set the OBSCONDITIONS mask for each target bit.
+    obscon = set_obsconditions(targets)
+
+    # ADM set up the output mtl table.
+    mtl = Table(targets)
+    mtl.meta['EXTNAME'] = 'MTL'
+    # ADM any target that wasn't matched to the ZCAT should retain its
+    # ADM original (INIT) value of PRIORITY and NUMOBS.
+    mtl['NUMOBS_MORE'] = mtl['NUMOBS_INIT']
+    mtl['PRIORITY'] = mtl['PRIORITY_INIT']
+    # ADM now populate the new mtl columns with the updated information.
+    mtl['OBSCONDITIONS'] = obscon
+    mtl['PRIORITY'][zmatcher] = priority
+    mtl['NUMOBS_MORE'][zmatcher] = ztargets['NUMOBS_MORE']
+
+    # Filter out any targets marked as done.
+    if trim:
+        notdone = mtl['NUMOBS_MORE'] > 0
+        log.info('{:d} of {:d} targets are done, trimming these'.format(
+            len(mtl) - np.sum(notdone), len(mtl))
+        )
+        mtl = mtl[notdone]
+
+    # Filtering can reset the fill_value, which is just wrong wrong wrong
+    # See https://github.com/astropy/astropy/issues/4707
+    # and https://github.com/astropy/astropy/issues/4708
+    mtl['NUMOBS_MORE'].fill_value = -1
+
+    return mtl
